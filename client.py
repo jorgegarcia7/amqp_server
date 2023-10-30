add3e99d703bfc8e58121e2ba01913135f6e4491
jagarcia7 <jgarciam95@gmail.com>
2023-10-30 17:57:05 +0100

    - The Server uses now prefetch_count=1 and acknowledges the message when it is received, before processing it. - The Server is able to process multiple messages asynchronously, using asyncio.create_task(). - Added correlation_id functionality to match responses to specific requests (commands). - A client() can now send multiple commands using the same queue, instead of just one.

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import time
 import uuid
 from concurrent import futures
 
@@ -7,18 +8,26 @@ import pika
 import json
 
 
+pending_responses = {}
+
+
 def on_response(ch, method, properties, body):
     """
-    Callback function when a response is received. It prints the response and exits.
+    Callback function when a response is received. It prints the response and exits if no there are no pending
+    commands to be responded.
     """
-    print(f"Response: {body.decode()}")
-    ch.stop_consuming()
+    if properties.correlation_id in pending_responses:
+        print(f"Response: {body.decode()}. Correlation_id: {properties.correlation_id}")
+        del pending_responses[properties.correlation_id]
+
+        if not pending_responses:
+            ch.stop_consuming()
 
 
-def client(command, body_json):
+def client(commands):
     """
     Synchronous client to test server.py
-    It publishes the message to the exchange created by the Server, waits for the Server's response and exits when
+    It publishes the commands to the exchange created by the Server, waits for the Server's response and exits when
     the response is received.
     Each invocation of client() will have its own exclusive queue to avoid consuming other clients' messages.
     The client implements a timeout, so if no response is received by the server then it exits.
@@ -39,18 +48,25 @@ def client(command, body_json):
         queue=callback_queue, on_message_callback=on_response, auto_ack=True
     )
 
-    channel.basic_publish(
-        exchange=exchange_name,
-        routing_key=command,
-        properties=pika.BasicProperties(reply_to=callback_queue),
-        body=json.dumps(body_json).encode("utf-8"),
-    )
+    for message in commands:
+        command = message[0]
+        body_json = message[1]
 
-    print(f"Sent '{command, body_json}'")
+        corr_id = str(uuid.uuid4())
+        pending_responses[corr_id] = None
+
+        channel.basic_publish(
+            exchange=exchange_name,
+            routing_key=command,
+            properties=pika.BasicProperties(reply_to=callback_queue, correlation_id=corr_id),
+            body=json.dumps(body_json).encode("utf-8"),
+        )
+
+        print(f"Sent '{command, body_json}. Correlation_id: {corr_id}'")
 
     # Using a loop with channel.consume() instead of channel.start_consuming() to be able to implement an
     # inactivity_timeout.
-    timeout_seconds = 20
+    timeout_seconds = 60
     for method_frame, properties, body in channel.consume(
         callback_queue, inactivity_timeout=timeout_seconds
     ):
@@ -66,16 +82,19 @@ if __name__ == "__main__":
     # set logging to DEBUG/INFO if we want to see more details
     logging.basicConfig(level=logging.WARNING)
 
+    start_time = time.time()
+
+    messages = (("SET", {"key": "one", "value": 1}), ("GET", {"key": "one"}))
     processes = os.cpu_count()
 
     _process_jobs = []
     with futures.ProcessPoolExecutor(max_workers=processes) as p:
         for i in range(10):
-            job = p.submit(client, "SET", {"key": "one", "value": 1})
-            _process_jobs.append(job)
-            job = p.submit(client, "GET", {"key": "one"})
+            job = p.submit(client, messages)
             _process_jobs.append(job)
 
     for job in futures.as_completed(_process_jobs):
         job.result()  # wait for result
 
+    end_time = time.time()
+    logging.warning(f'It took {end_time - start_time} seconds to execute all the commands.')
--- server.py
+++ server.py
@@ -4,9 +4,9 @@ import logging
 import aio_pika
 
 
-async def process_message(message, channel, key_value_dict):
+async def execute_command(command, data_dict, key_value_dict):
     """
-    Processes the messages that come from the client and sends back a response.
+    Execute the given command and return the response.
     Clients can get the value of a key, set the value of a key, and delete a key-value pair. Examples:
     "GET": Example JSON: {"key": "tralala"}
     "SET": Example JSON: {"key": "tralala", "value": 123}
@@ -15,48 +15,67 @@ async def process_message(message, channel, key_value_dict):
     Note that "delete" will return an error if the key is not present.
 
     """
-    async with message.process():  # to be able to send the acknowledgement of the message
-        command = message.routing_key
-        data_dict = json.loads(message.body)
-        logging.info(f" Received message {command, data_dict}")
+    response = {}
 
-        response = {}
+    if command == "SET" and set(data_dict.keys()) != {"key", "value"}:
+        response["error"] = "Wrong JSON format. Example JSON: {'key': 'tralala', 'value': 123}"
 
-        if command == "SET" and set(data_dict.keys()) != {"key", "value"}:
-            response["error"] = "Wrong JSON format. Example JSON: {'key': 'tralala', 'value': 123}"
+    elif command in ["GET", "DELETE"] and set(data_dict.keys()) != {"key"}:
+        response["error"] = "Wrong JSON format. Example JSON: {'key': 'tralala'}"
 
-        elif command in ["GET", "DELETE"] and set(data_dict.keys()) != {"key"}:
-            response["error"] = "Wrong JSON format. Example JSON: {'key': 'tralala'}"
+    else:
+        try:
+            if command == "GET":
+                await asyncio.sleep(1)  # simulating "GET" commands take 1 second of I/O operation
+                response["value"] = key_value_dict[data_dict["key"]]
+            elif command == "SET":
+                await asyncio.sleep(5)  # simulating "SET" commands take 5 seconds of I/O operation
+                key_value_dict[data_dict["key"]] = data_dict["value"]
+                response["status"] = "OK"
+            elif command == "DELETE":
+                del key_value_dict[data_dict["key"]]
+                response["status"] = "OK"
+            else:
+                response["error"] = f"Invalid command: {command}"
+        except KeyError as e:
+            response["error"] = f"Key error: {str(e)}"
+        except Exception as e:
+            response["error"] = str(e)
 
-        else:
-            try:
-                if command == "GET":
-                    await asyncio.sleep(1)
-                    response["value"] = key_value_dict[data_dict["key"]]
-                elif command == "SET":
-                    await asyncio.sleep(5)
-                    key_value_dict[data_dict["key"]] = data_dict["value"]
-                    response["status"] = "OK"
-                elif command == "DELETE":
-                    del key_value_dict[data_dict["key"]]
-                    response["status"] = "OK"
-                else:
-                    response["error"] = f"Invalid command: {command}"
-            except KeyError as e:
-                response["error"] = f"Key error: {str(e)}"
-            except Exception as e:
-                response["error"] = str(e)
+    return response
 
-        response_message = aio_pika.Message(body=json.dumps(response).encode())
 
-        # Publish the response to the reply_to queue specified by the client:
-        if message.reply_to:
-            logging.info(f" Sending response to {message.reply_to}: {response}")
-            await channel.default_exchange.publish(
-                response_message, routing_key=message.reply_to
-            )
+async def process_message(message, channel, key_value_dict):
+    """
+    Processes the message that comes from the client and sends back a response.
 
-        logging.info(" Done")
+    """
+    # Acknowledge the message immediately:
+    await message.ack()
+
+    command = message.routing_key
+    data_dict = json.loads(message.body)
+    corr_id = message.correlation_id
+    logging.info(f" Received message {command, data_dict}")
+
+    response = await execute_command(command, data_dict, key_value_dict)
+    response_message = aio_pika.Message(body=json.dumps(response).encode(), correlation_id=corr_id)
+
+    # Publish the response to the reply_to queue specified by the client:
+    if message.reply_to:
+        logging.info(f" Sending response to {message.reply_to}: {response}")
+        await channel.default_exchange.publish(
+            response_message, routing_key=message.reply_to
+        )
+
+    logging.info(" Done")
+
+
+async def consume_message(message, channel, key_value_dict):
+    """
+    Create a separate asynchronous task for each message to be processed concurrently.
+    """
+    asyncio.create_task(process_message(message, channel, key_value_dict))
 
 
 async def server():
@@ -71,6 +90,9 @@ async def server():
     connection = await aio_pika.connect_robust("amqp://guest:guest@localhost/")
     channel = await connection.channel()
 
+    # Forcing prefetch_count=1 to test that the server is truly asynchronous:
+    await channel.set_qos(prefetch_count=1)
+
     exchange_name = "norbit_exchange"
     exchange = await channel.declare_exchange(
         exchange_name, aio_pika.ExchangeType.TOPIC
@@ -80,7 +102,7 @@ async def server():
     # "#" matches any routing_key (works with ExchangeType.TOPIC)
     await queue.bind(exchange, routing_key="#")
     await queue.consume(
-        lambda message: process_message(message, channel, key_value_dict)
+        lambda message: consume_message(message, channel, key_value_dict)
     )
 
     try:
